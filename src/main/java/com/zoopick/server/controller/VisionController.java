9dec8a3bf48a909a736e0236d86667e7d13210a5
Keonwoo <dica0829@naver.com>
2026-04-29 16:33:12 +0900

    Fix: VisionController RequestMapping 경로 슬래시 누락 수정

--- src/main/java/com/zoopick/server/controller/VisionController.java
+++ src/main/java/com/zoopick/server/controller/VisionController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/vision")
+@RequestMapping("/api/vision")
 @RequiredArgsConstructor
 public class VisionController {
     private final VisionService visionService;

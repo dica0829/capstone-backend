24802fb63f1e17d35ce50125e36e2ca7c1c899f0
Keonwoo <dica0829@naver.com>
2026-05-03 17:52:07 +0900

    Feat: CCTV 분석 상태 관리 엔티티 및 DTO 정의 - 비디오 분석 진행률 및 상태 추적을 위한 CctvVideoProgress 엔티티 추가 - FastAPI와의 통신 규약 변경에 따른 신규 Callback DTO 구조 설계 - 불필요해진 구형 DTO 및 관련 클래스 삭제

---
+++ src/main/java/com/zoopick/server/dto/cctv/CctvProgressCallback.java
@@ -0,0 +1,32 @@
+package com.zoopick.server.dto.cctv;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CctvProgressCallback {
+    @JsonProperty("video_id")
+    private Long videoId;
+
+    private String status;
+
+    @JsonProperty("analyzed_seconds")
+    private Integer analyzedSeconds;
+
+    @JsonProperty("total_seconds")
+    private Integer totalSeconds;
+
+    @JsonProperty("progress_percent")
+    private Double progressPercent;
+
+    @JsonProperty("estimated_completion_at")
+    private LocalDateTime estimatedCompletionAt;
+}
